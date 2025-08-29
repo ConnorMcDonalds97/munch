2875386b376202c668a7d3416c7e413c0444e4d5
kevin <kcao36@outlook.com>
2025-08-29 00:30:02 -0600

    start swiper

--- UberEat/app/game.jsx
+++ UberEat/app/game.jsx
@@ -1,12 +1,34 @@
-import { View, Text, StyleSheet, Pressable} from 'react-native'
+import { View, Text, StyleSheet, Pressable, Image} from 'react-native'
 import React from 'react'
 import { Link } from "expo-router"
+import Swiper from 'react-native-deck-swiper'
+import data from './data'
+
+const Card = ({card}) => (
+  <View style={styles.card}>
+    <Image source={{uri: card.image}} style={styles.cardImage}/>
+  </View>
+)
 
 const App = () => {
+  const [index, setIndex] = React.useState(0)
   return (
     <View style = {styles.container}>
-      {/*title*/}
-      <Text style={styles.title}>if you're reading this, the button worked Game to be hosted here.</Text>  
+      <Swiper
+  cards={data}
+  cardIndex={index}
+  renderCard={(card) => {
+    if (!card) {
+      return (
+        <View style={[styles.card, {backgroundColor: 'gray'}]}>
+          <Text style={{color: 'white'}}>No Card</Text>
+        </View>
+      );
+    }
+    return <Card card={card} />;
+  }}
+/>
+
     </View>
   )
 }
@@ -16,41 +38,24 @@ export default App
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: "black"
-  },
-  title: {
-    color:'white',
-    fontSize: 42,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    backgroundColor: 'rgba(0,0,0,0.5)',
-    marginBottom: 120
-  },
-  link: {
-    color:'white',
-    fontSize: 42,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    padding: 4,
-    textDecoration: 'none'
-  },
-  button: {
-    backgroundColor: 'white',
-    borderRadius: 16,
-    marginHorizontal: 'auto',
-    padding: 4,
-    width: 100,
+    backgroundColor: "black",
     alignItems: 'center',
     justifyContent: 'center'
   },
-  buttonText:{
-    color: 'white',
-    fontSize: 36,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    padding: 4,
-    textDecorationLine: 'none'
+  card: {
+    borderRadius: 8,
+    shadowRadius: 25,
+    shadowColor: "black",
+    shadowOpacity: 0.08,
+    shadowOffset: {width: 0, height: 0},
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "#fff"
+  },
+  cardImage: {
+    width: 160,
+    flex: 1,
+    resizeMode: 'contain'
   }
-
+  
 })
